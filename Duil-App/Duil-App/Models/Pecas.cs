9cf33d025852b72ba823a7bb9325561a859a70f3
ClaudioJorge24871 <aluno24871@ipt.pt>
2025-04-18 15:54:33 +0100

    Anotações no modelo Peças

--- Duil-App/Duil-App/Models/Pecas.cs
+++ Duil-App/Duil-App/Models/Pecas.cs
@@ -12,21 +12,27 @@ namespace Duil_App.Models
         /// Referência da peça
         /// </summary>
         [Key]
+        [Display(Name = "Referência")]
         public int Referencia { get; set; }
-        
+
         /// <summary>
         /// Designação da Peça. Simples nome ou descrição
         /// </summary>
+        [StringLength(100)]
+        [Display(Name = "Designação")]
         public string? Designacao { get; set; }
 
         /// <summary>
         /// Preco unitário
         /// </summary>
+        [Display(Name = "Preço unitário")]
+        [Range(0.01, double.MaxValue, ErrorMessage = "O preço unitário deve ser maior que zero.")]
         public decimal PrecoUnit { get; set; }
 
         /// <summary>
         /// Fabricante
         /// </summary>
+        [Required(ErrorMessage = "A identificação da fábrica é obrigatório")]
         public int FabricaId { get; set; }
 
         public required Fabricas Fabrica { get; set; }
