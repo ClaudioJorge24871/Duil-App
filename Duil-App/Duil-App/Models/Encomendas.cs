505206eeb5ed12a689cf3c1e11b5937ea9fbb197
ClaudioJorge24871 <aluno24871@ipt.pt>
2025-04-18 15:39:57 +0100

    Validação dos dados do Modelo Encomendas

--- Duil-App/Duil-App/Models/Encomendas.cs
+++ Duil-App/Duil-App/Models/Encomendas.cs
@@ -16,26 +16,36 @@ namespace Duil_App.Models
         /// <summary>
         /// Identificador da encomenda do lado da empresa cliente
         /// </summary>
+        [Display(Name = "Identificador da encomenda - lado Cliente")]
         public int? IdLadoCliente { get; set; }
 
         /// <summary>
         /// Data de realização da encomenda
         /// </summary>
+        [Display (Name = "Data")]
+        [DataType(DataType.Date)]
+        [Range(typeof(DateTime), "01/01/2000", "01/01/2099", ErrorMessage = "A data deve estar entre 01/01/2000 e 01/01/2099.")]
         public DateTime Data { get; set; }
 
         /// <summary>
-        /// Total do preço da encomenda
+        /// Total do preço da encomenda 
         /// </summary>
+        [Display(Name = "Preço Total")]
+        [Range(0.01, double.MaxValue, ErrorMessage = "O preço total deve ser maior que zero.")]
         public decimal TotalPrecoUnit { get; set; }
 
         /// <summary>
         /// Quantidade total de peças na encomenda
         /// </summary>
+        [Display(Name = "Quantidade Total")]
+        [Range(1, int.MaxValue, ErrorMessage = "A quantidade total deve ser maior que zero.")]
         public int QuantidadeTotal { get; set; }
 
         /// <summary>
         /// Nome da transportadora que efetua a entrega
         /// </summary>
+        [Display(Name = "Empresa transportadora")]
+        [StringLength(100)]
         public string? Transportadora { get; set; }
 
         /// <summary>
@@ -43,13 +53,17 @@ namespace Duil_App.Models
         /// </summary>
         public Estados Estado { get; set; }
 
+
+
         /// <summary>
         /// Identificação da empresa cliente 
         /// </summary>
+        [Required(ErrorMessage = "O cliente é obrigatório.")]
+        [Display(Name = "Identificador do Cliente")]
         public int ClienteId { get; set; }
         public required Clientes Cliente { get; set; }
 
-        
+
         public ICollection<LinhaEncomenda>? LinhasEncomenda { get; set; }
     }
 
