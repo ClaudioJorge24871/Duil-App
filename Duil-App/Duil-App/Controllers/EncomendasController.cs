98d899ef8618f77a7bc6c3a4e85e4832f9f1f03b
ClaudioJorge24871 <aluno24871@ipt.pt>
2025-06-12 17:17:43 +0100

    feat: Primeira implementação da autorização na criação de encomendas:
    - Clientes podem criar encomendas com as peças a qual estão associados
    - Admin e Funcionarios podem Criar Encomendas com Pecas que estão associadas ao Cliente selecionado

--- Duil-App/Duil-App/Controllers/EncomendasController.cs
+++ Duil-App/Duil-App/Controllers/EncomendasController.cs
@@ -9,19 +9,23 @@ using Duil_App.Data;
 using Duil_App.Models;
 using System.Globalization;
 using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
 
 namespace Duil_App.Controllers
 {
-    [Authorize(Roles = "Admin,Funcionario")]
+    
     public class EncomendasController : Controller
     {
         private readonly ApplicationDbContext _context;
+        private readonly UserManager<Utilizadores> _userManager;
 
-        public EncomendasController(ApplicationDbContext context)
+        public EncomendasController(ApplicationDbContext context, UserManager<Utilizadores> userManager)
         {
             _context = context;
+            _userManager = userManager;
         }
 
+        [Authorize(Roles = "Admin,Funcionario")]
         public async Task<IActionResult> Index()
         {
             return View(await _context.Encomendas
@@ -62,9 +66,28 @@ namespace Duil_App.Controllers
             {
                 try
                 {
-                    var pecas = await _context.Pecas
-                        .Where(p => pecasSelecionadas.Contains(p.Id))
-                        .ToListAsync();
+                    List<Pecas> pecas = new();
+
+                    if (User.IsInRole("Cliente")) // Se for CLiente apenas pode criar encomendas com as suas Pecas
+                    {
+                        var userId = _userManager.GetUserId(User);
+                        pecas = await _context.Pecas
+                            .Where(p => p.ClienteId == userId && pecasSelecionadas.Contains(p.Id))
+                            .ToListAsync();
+                    }
+                    else if (User.IsInRole("Funcionario") || User.IsInRole("Admin")) // Se for Funcionario ou Admin pode criar encomendas com Pecas associadas ao cliente colocado
+                    {
+                        pecas = await _context.Pecas
+                            .Where(p => pecasSelecionadas.Contains(p.Id))
+                            .ToListAsync();
+                    }
+
+                    if (pecas.Count == 0)
+                    {
+                        ModelState.AddModelError("", "Não foram encontradas peças válidas para a encomenda.");
+                        return View(encomenda);
+                    }
+
 
                     // Manual decimal parsing to handle culture differences
                     encomenda.QuantidadeTotal = quantidades.Sum();
@@ -243,6 +266,22 @@ namespace Duil_App.Controllers
                 .ToListAsync();
 
             return Json(pecas);
+        }
+
+        /// <summary>
+        /// Buscar as encomendas associadas ao utilizador autenticado.
+        /// </summary>
+        /// <returns>Lista de Encomendas</returns>
+        public async Task<IActionResult> GetClientesEncomendas()
+        {
+            var userId = _userManager.GetUserId(User);
+
+            var encomendas = await _context.Encomendas
+                .Where(e => e.ClienteId == userId)
+                .Include(e => e.LinhasEncomenda)
+                .ToListAsync();
+
+            return View(encomendas);
         }
     }
 }
