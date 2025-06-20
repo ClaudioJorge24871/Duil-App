af51a4ee11205f9da2439e44f79bbd31fb0f05d0
ClaudioJorge24871 <aluno24871@ipt.pt>
2025-06-20 19:17:16 +0100

    feat: Funcionario e Cliente como utilizadores seed

--- Duil-App/Duil-App/Models/ApplicationDBInit.cs
+++ Duil-App/Duil-App/Models/ApplicationDBInit.cs
@@ -7,12 +7,18 @@
 
     public class ApplicationDBInit
     {
+        /// <summary>
+        /// Seed da base de dados, com roles e utilizadores iniciais
+        /// </summary>
+        /// <param name="serviceProvider"></param>
+        /// <param name="config"></param>
+        /// <returns></returns>
         public static async Task SeedAsync(IServiceProvider serviceProvider, IConfiguration config)
         {
             var roleManager = serviceProvider.GetRequiredService<RoleManager<IdentityRole>>();
             var userManager = serviceProvider.GetRequiredService<UserManager<Utilizadores>>();
 
-            string[] roles = { "Admin", "Cliente", "Funcionario", "Utilizador"};
+            string[] roles = { "Admin", "Cliente", "Funcionario", "Utilizador"}; // Roles
 
             foreach (var role in roles)
             {
@@ -22,9 +28,9 @@
                 }
             }
 
-            // Criar admin se nao existe
+            // Criar admin
             string adminEmail = config["AdminUser:Email"] ?? " ";
-            string adminPassword = config["AdminUser:Password"] ?? " "; // Possivel mudar para tornar mais seguro
+            string adminPassword = config["AdminUser:Password"] ?? " ";
             var adminUser = await userManager.FindByEmailAsync(adminEmail);
 
             if (adminUser == null)
@@ -42,6 +48,52 @@
                 if (result.Succeeded)
                 {
                     await userManager.AddToRoleAsync(user, "Admin");
+                }
+            }
+
+            // Criar Funcionario
+            string funcionarioEmail = "funcionario@duil.com";
+            string funcionarioPassword = "Funcionario@1234";
+            var funcionarioUser = await userManager.FindByEmailAsync(funcionarioEmail);
+
+            if (funcionarioUser == null)
+            {
+                var user = new Utilizadores
+                {
+                    UserName = funcionarioEmail,
+                    Email = funcionarioEmail,
+                    EmailConfirmed = true,
+                    Nome = "Funcionario",
+                    Pais = "Portugal",
+                };
+
+                var result = await userManager.CreateAsync(user, funcionarioPassword);
+                if (result.Succeeded)
+                {
+                    await userManager.AddToRoleAsync(user, "Funcionario");
+                }
+            }
+
+            // Criar Cliente
+            string clienteEmail = "cliente@duil.com";
+            string clientePassword = "Cliente@1234";
+            var clienteUser = await userManager.FindByEmailAsync(clienteEmail);
+
+            if (clienteUser == null)
+            {
+                var user = new Utilizadores
+                {
+                    UserName = clienteEmail,
+                    Email = clienteEmail,
+                    EmailConfirmed = true,
+                    Nome = "Cliente",
+                    Pais = "Portugal",
+                };
+
+                var result = await userManager.CreateAsync(user, clientePassword);
+                if (result.Succeeded)
+                {
+                    await userManager.AddToRoleAsync(user, "Cliente");
                 }
             }
 
