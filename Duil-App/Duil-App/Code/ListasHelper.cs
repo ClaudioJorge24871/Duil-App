269f920cf2848539fcb0fc20797b7595f4de691f
ClaudioJorge24871 <aluno24871@ipt.pt>
2025-06-27 10:58:04 +0100

    adicionado o tipo "Outro" à lista de paises

--- Duil-App/Duil-App/Code/ListasHelper.cs
+++ Duil-App/Duil-App/Code/ListasHelper.cs
@@ -3,8 +3,15 @@ using System.Collections.Generic;
 
 namespace Duil_App.Code
 {
+    /// <summary>
+    /// Helper Lista de Países para ser utilizado em outras partes da app
+    /// </summary>
     public static class ListasHelper
     {
+        /// <summary>
+        /// Obtêm a Lista dos Países disponíveis
+        /// </summary>
+        /// <returns></returns>
         public static List<SelectListItem> ObterListaDePaises()
         {
             return new List<SelectListItem>
@@ -14,7 +21,8 @@ namespace Duil_App.Code
                 new SelectListItem { Value = "França", Text = "França" },
                 new SelectListItem { Value = "Holanda", Text = "Holanda" },
                 new SelectListItem { Value = "Inglaterra", Text = "Inglaterra" },
-                new SelectListItem { Value = "Suecia", Text = "Suécia"}
+                new SelectListItem { Value = "Suecia", Text = "Suécia"},
+                new SelectListItem { Value = "Outro", Text = "Outro"} // Fallback de País. CAso outro cliente. Não deveria ser utilizado
             };
         }
     }
