2e02d0a27f85a9e27637c9e70e2ed4218cffdb8c
ClaudioJorge24871 <aluno24871@ipt.pt>
2025-06-29 02:29:56 +0100

    fix: Caso Pecas tenham encomendas associadas a elas, não remove a peça e fornece um motivo.
    Isto evita problemas com a base de dados

--- Duil-App/Duil-App/Controllers/PecasController.cs
+++ Duil-App/Duil-App/Controllers/PecasController.cs
@@ -307,10 +307,21 @@ namespace Duil_App.Controllers
             var peca = await _context.Pecas.FindAsync(id);
             if (peca != null)
             {
+                // Se a Peça tiver encomendas associadas a elas, não deve poder apagar
+                bool temEncomendas = await _context.LinhasEncomendas
+            .AnyAsync(le => le.PecaId == id);
+
+                if (temEncomendas)
+                {
+                    // Mostra uma mensagem de erro
+                    TempData["ErrorMessage"] = "Não foi possível remover esta peça. Motivo: Existem encomendas associadas à mesma.";
+                    return RedirectToAction(nameof(Index));
+                }
+
                 _context.Pecas.Remove(peca);
+                await _context.SaveChangesAsync();
             }
 
-            await _context.SaveChangesAsync();
             return RedirectToAction(nameof(Index));
         }
 
@@ -318,7 +329,6 @@ namespace Duil_App.Controllers
         {
             return _context.Pecas.Any(e => e.Id == id);
         }
-
 
 
     }
